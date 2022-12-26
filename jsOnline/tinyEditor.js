e523fcf33b17dac121e17a3ebf2f971581fd781e
刘lyxAndy <65491750+liulyxandy-codemao@users.noreply.github.com>
2022-12-26 08:09:27 +0800

    Create tinyEditor.js

---
+++ jsOnline/tinyEditor.js
@@ -0,0 +1,20 @@
+/* tinyEditor one-step install */
+/* by LiuLyxAndy */
+
+// Editor Load
+var v = document.createElement("script")
+v.setAttribute("src","https://cdn.tiny.cloud/1/7qgj71g0z3sd3jyvrzda07h5ig9080ol68v1znh908rzveiw/tinymce/6/tinymce.min.js")
+document.getElementsByTagName("body")[0].appendChild(v)
+
+var q=true
+while(q){
+  try{
+    tinymce.init({[
+      selector: 'textarea#default-editor'
+      ]
+    });
+    q=false;
+  }catch(e){
+  console.log(e)
+  }
+}
