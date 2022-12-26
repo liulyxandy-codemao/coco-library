7f9030a50824759c128a1f45d75856c2ee8f5a60
刘lyxAndy <65491750+liulyxandy-codemao@users.noreply.github.com>
2022-12-26 08:34:37 +0800

    Update tinyEditor.js

--- jsOnline/tinyEditor.js
+++ jsOnline/tinyEditor.js
@@ -5,7 +5,7 @@
 var v = document.createElement("script")
 v.setAttribute("src","https://cdn.tiny.cloud/1/7qgj71g0z3sd3jyvrzda07h5ig9080ol68v1znh908rzveiw/tinymce/6/tinymce.min.js")
 document.getElementsByTagName("body")[0].appendChild(v)
-
+/*
 var q=true
 while(q){
 
@@ -16,3 +16,4 @@ while(q){
     q=false;
     
 }
+*/
